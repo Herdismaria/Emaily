365aecde7a8e76bed5923b123d531df8947a3c96
Herdis Maria <herdismaria@Herdiss-MacBook-Pro.local>
2018-01-09 09:33:48 +0100

    file refactor finished

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+
+// runs the code in the passport file
+require('./services/passport');
 
 // setup configuration to listen for requests
 const app = express();
+
+// Attach the route handlers to the app object
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 const passport = require('passport');
 
-// When request comes in, hand it to passport
-app.get(
-  '/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] })
-);
+module.exports = app => {
+  // When request comes in, hand it to passport
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+  );
 
-app.get('/auth/google/callback', passport.authenticate('google'));
-
+  app.get('/auth/google/callback', passport.authenticate('google'));
+};
